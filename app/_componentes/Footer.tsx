160989a865cd2a37be78bc153aef540a50e92088
Gabriel Costa <107140329+gabrieltacosta@users.noreply.github.com>
2025-09-18 11:20:25 -0300

    Update Footer.tsx

--- app/_componentes/Footer.tsx
+++ app/_componentes/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="flex flex-col items-center justify-center w-full h-16 bg-[#2C2D1C] px-4 md:px-20">
       <div className="flex w-full items-center justify-center border-t border-[#7A7A7A]">
         <span className="text-[#7a7a7a] text-xs md:text-sm lg:text-xl italic">
-          &copy; {new Date().getFullYear()} Studio Garden Brows - Todos os
+          &copy; {new Date().getFullYear()} Studio Garden Brows®️ - Todos os
           Direitos Reservados.
         </span>
       </div>
