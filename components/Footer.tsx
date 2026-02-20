cefdedac710d48d01e30c20f18d049e96af3f9dd
gabrieltacosta <gabriel.tuka@gmail.com>
2026-02-20 14:28:18 -0300

    update: package

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="flex flex-col items-center justify-center w-full h-16 bg-[#2C2D1C] px-4 md:px-20">
       <div className="flex w-full items-center justify-center border-t border-[#7A7A7A]">
-        <span className="text-[#7a7a7a] text-xs md:text-sm lg:text-xl italic">
+        <span className="text-[#7a7a7a] text-xs md:text-sm lg:text-lg italic">
           &copy; {new Date().getFullYear()} Studio Garden Brows&reg; - Todos os
           Direitos Reservados.
         </span>
