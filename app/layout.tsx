341514d93ac450e0336b1bd3e86fa34136efdf7c
Gabriel Costa <107140329+gabrieltacosta@users.noreply.github.com>
2026-08-26 10:48:09 -0300

    Fix language attribute format in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt_BR">
+    <html lang="pt-BR">
       <body
         className={`${roboto.className} antialiased w-full bg-[url('/background_mobile.png')] lg:bg-[url('/background.png')] bg-center bg-cover bg-no-repeat`}
         cz-shortcut-listen="true"
